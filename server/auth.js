0cac348b4ac6c62febb6c0d326ff9a3260c9ab99
Benoit Hirbec <benoit.hirbec@gmail.com>
2017-05-13 15:08:24 -0700

    Fetch more information from linkedin API

--- server/auth.js
+++ server/auth.js
@@ -53,35 +53,77 @@ function retrieveAccessToken(res, code) {
             return
         }
 
-        // access_token
-        // expires_in
         getUserId(res, token)
     })
 }
 
 function getUserId(res, token) {
+    let url = "https://api.linkedin.com/v1/people/~:("
+        + "id,"
+        + "email-address,"
+        + "first-name,"
+        + "last-name,"
+        + "maiden-name,"
+        + "formatted-name,"
+        + "phonetic-first-name,"
+        + "phonetic-last-name,"
+        + "formatted-phonetic-name,"
+        + "headline,"
+        + "location,"
+        + "industry,"
+        + "current-share,"
+        + "num-connections,"
+        + "num-connections-capped,"
+        + "summary,"
+        + "specialties,"
+        + "positions:("
+            + "id,"
+            + "title,"
+            + "summary,"
+            + "start-date,"
+            + "end-date,"
+            + "is-current,"
+            + "company:("
+                + "id,"
+                + "name,"
+                + "type,"
+                + "size,"
+                + "industry,"
+                + "ticker)"
+        + "),"
+        + "picture-url,"
+        + "picture-urls::(original),"
+        + "site-standard-profile-request,"
+        + "api-standard-profile-request,"
+        + "public-profile-url"
+    + ")?format=json"
+
     let reqOptions = {
-        url: 'https://api.linkedin.com/v1/people/~:(id,first-name,last-name)?format=json',
+        url: url,
         headers: {'Authorization': 'Bearer ' + token.access_token},
         json: true
     }
 
-    request.get(reqOptions, function(err, httpResponse, user) {
+    request.get(reqOptions, function(err, httpResponse, data) {
         if (err != undefined) {
             res.send(500, err)
             return
         }
 
-        let data = {token: token, user: user}
-        fb.ref("/users").child(user.id).set(data)
-        res.send(JSON.stringify(user))
+        if (data.errorCode != undefined) {
+            res.send(data.status, data.message)
+            return
+        }
+
+        fb.ref('/users').child(data.id).set({token: token, user: data})
+        res.send(JSON.stringify(data))
     })
 }
 
-app.get("/auth/accept", function (req, res) {
+app.get('/auth/accept', function (req, res) {
     res.send('Hello World!')
 })
 
-app.get("/auth/cancel", function (req, res) {
+app.get('/auth/cancel', function (req, res) {
     res.send('Hello World!')
 })

4be49b047b4706c6609aade80c7aa5828b28e134
Benoit Hirbec <benoit.hirbec@gmail.com>
2017-05-14 23:32:22 -0700

    Add experiences to profile

--- app/profile.jsx
+++ app/profile.jsx
@@ -2,10 +2,26 @@
     <div className="container">
         <div className="row">
             <div className="clearfix"></div>
-            <h2 className="section-heading">{this.props.firstName} {this.props.lastName}</h2>
-            <p style={{'fontWeight': 'normal'}}>
-                {this.props.summary}
-            </p>
+            <h1 className="section-heading">
+                {this.props.firstName} {this.props.lastName}
+            </h1>
+            <p>{this.props.summary}</p>
+        </div>
+
+        <div className="row">
+            <h2>Experience</h2>
+
+            {this.props.positions.values.map(function (p, i) {
+                let key = 'position-' + i
+                return <div>
+                    <h3>{p.title} @ {p.company.name}</h3>
+                    <div key={key}>
+                        {(p.summary || '').split('\n').map(function (v, j) {
+                            return <div key={key + '-' + j}>{v}</div>
+                        })}
+                    </div>
+                </div>
+            })}
         </div>
     </div>
 </div>

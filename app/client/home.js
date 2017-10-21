bfb52656efe95bb1afc826fb4022cc05e4325a58
Benoit Hirbec <benoit.hirbec@gmail.com>
2017-10-20 22:46:20 -0700

    Reagan's home page version

--- app/client/home.js
+++ app/client/home.js
@@ -8,12 +8,9 @@ class Home extends React.Component {
             <div className="intro-header">
                 <div className="container intro-message">
                     <h1>
-                        Your résumé, <span className="main-color">built by the crowd.</span>
+                        Let's show the world,<br />
+                        <span className="main-color">How awesome you are</span>
                     </h1>
-                    <h2>
-                        Create an authentic resume with the world's first<br />
-                        crowd-sourced résumé building platform.
-                    </h2>
 
                     <Link to='/signup'>
                         <button
@@ -30,38 +27,57 @@ class Home extends React.Component {
                 <div className="container media-container-row">
                     <div className="card p-3 col-12 col-md-6 col-lg-4">
                         <div className="card-box">
-                            <h4 className="card-title py-3 mbr-fonts-style display-5 main-color">Never write your résumé again!</h4>
-                            <p className="mbr-text mbr-fonts-style display-7">In just minutes, get up and running with a powerful résumé, that highlights your work experiences and unique skills.&nbsp;</p>
+                            <i className="material-icons">access_time</i>
+                            <h4 className="card-title py-3 mbr-fonts-style display-5">
+                                #SaveTime
+                                <div className="main-color">Don't write your resume.</div>
+                            </h4>
+                            <p className="mbr-text mbr-fonts-style display-7">
+                                Just fill in the basics. Your network will finish it for you!
+                            </p>
                         </div>
                     </div>
 
                     <div className="card p-3 col-12 col-md-6 col-lg-4">
                         <div className="card-box">
-                            <h4 className="card-title py-3 mbr-fonts-style display-5 main-color">Receive reviews &amp; endorsements from your trusted network&nbsp;</h4>
+                            <i className="material-icons">lightbulb_outline</i>
+                            <h4 className="card-title py-3 mbr-fonts-style display-5">
+                                #ShowOff
+                                <div className="main-color">Let's show you off.</div>
+                            </h4>
                             <p className="mbr-text mbr-fonts-style display-7">
-                                Invite colleagues, employers, advisors, professors &amp; clients to write supportive reviews of your projects, skills &amp; capabilities.</p>
+                                Receive positive reviews & up-votes on your skills
+                                and abilities.
+                            </p>
                         </div>
                     </div>
 
                     <div className="card p-3 col-12 col-md-6 col-lg-4">
                         <div className="card-box">
-                            <h4 className="card-title py-3 mbr-fonts-style display-5 main-color">Showcase a résumé that stands out from the crowd!</h4>
+                            <i className="material-icons">thumb_up</i>
+                            <h4 className="card-title py-3 mbr-fonts-style display-5">
+                                #GetHired
+                                <div className="main-color">Well, that was easy.</div>
+                            </h4>
                             <p className="mbr-text mbr-fonts-style display-7">
-                                Radically transform your old résumé into a dynamic, authentic, online showcase that catapults you to new career heights.</p>
+                                Showcase a crowd-endorsed resume that gets
+                                you noticed!
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className="about-us">
+
+            {/*<div className="about-us">
                 <div className="container">
                     <div className="media-container-column mbr-white col-md-8">
                         <h3 className="main-color">
                             About Us
                         </h3>
                         <p className="mbr-text align-left pb-3 mbr-fonts-style display-5">
-                            Did you know that the <strong>average job seeker spends 1 to</strong>
-                            <strong>3 weeks writing their résumé</strong>? While the average&nbsp;
+                            Did you know that the <strong>average job seeker spends 1 to </strong>
+                            <strong>3 weeks writing their resume</strong>? While the average&nbsp;
                             <strong>Recruiter spends just <u>6</u> seconds reviewing it</strong>.
                         </p>
                         <p>
@@ -73,7 +89,7 @@ class Home extends React.Component {
                             Using design thinking, we've re-imagined the entire experience from the bottom-up,
                             to help millions of folks just like you around the world,&nbsp;
                             <strong>use the power of their network (and the crowd)</strong>,&nbsp;
-                            <strong>to write your résumé with you.</strong>
+                            <strong>to write your resume with you.</strong>
                         </p>
                         <p>
                             <strong>LetsResume is <u>free</u> to join</strong>, and is currently
@@ -81,7 +97,7 @@ class Home extends React.Component {
                         </p>
                     </div>
                 </div>
-            </div>
+            </div>*/}
 
             <div className="footer">
                 <div className="container">

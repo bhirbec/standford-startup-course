91ae1fd388f501dfe8b04c68030d7eb78022e2c8
Benoit Hirbec <benoit.hirbec@gmail.com>
2017-10-27 21:19:56 -0700

    Minor renames in functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const func = require('firebase-functions');
 
 const app = require('./app/server/app');
 const indexer = require('./app/server/algolia-indexer');
@@ -6,43 +6,43 @@ const liker = require('./app/server/liker');
 const mailer = require('./app/server/mailer');
 const messager = require('./app/server/messager');
 const user = require('./app/server/user');
+const db = func.database
 
+exports.app = func.https.onRequest(app);
 
-exports.app = functions.https.onRequest(app);
-
-exports.newInvite = functions.database.ref('invites/{inviteId}').onCreate((event) => {
-    return mailer.notifyInvite(event.data)
+exports.newInvite = db.ref('invites/{inviteId}').onCreate(e => {
+    return mailer.notifyInvite(e.data)
 });
 
 // TODO: delete profile
-exports.indexNewProfile = functions.database.ref('profile/{profileId}/view').onCreate((event) => {
-    return indexer.index(event.data)
+exports.indexNewProfile = db.ref('profile/{profileId}/view').onCreate(e => {
+    return indexer.index(e.data)
 });
 
 // This is triggered each time a profile change
-exports.indexUpdatedProfile = functions.database.ref('profile/{profileId}/view').onUpdate((event) => {
-    return indexer.index(event.data)
+exports.indexUpdatedProfile = db.ref('profile/{profileId}/view').onUpdate(e => {
+    return indexer.index(e.data)
 });
 
 // notify review
-exports.newReview = functions.database.ref('publicReviews/{revId}').onCreate((event) => {
-    return mailer.notifyReview(event.data)
+exports.newReview = db.ref('publicReviews/{revId}').onCreate(e => {
+    return mailer.notifyReview(e.data)
 });
 
-exports.updateReview = functions.database.ref('publicReviews/{revId}').onUpdate((event) => {
-    return mailer.notifyReview(event.data)
+exports.updateReview = db.ref('publicReviews/{revId}').onUpdate(e => {
+    return mailer.notifyReview(e.data)
 });
 
 // likes
-exports.hashtagLike = functions.database.ref('queue/like/{likeId}').onCreate((event) => {
-    return liker.likeHasktag(event.data)
+exports.hashtagLike = db.ref('queue/like/{likeId}').onCreate(e => {
+    return liker.likeHasktag(e.data)
 });
 
 // send Message
-exports.sendMessage = functions.database.ref('queue/message/{messageId}').onCreate((event) => {
-    return messager.sendMessage(event.data)
+exports.sendMessage = db.ref('queue/message/{messageId}').onCreate(e => {
+    return messager.sendMessage(e.data)
 });
 
 // user
-exports.onCreateUser = functions.auth.user().onCreate(user.onCreate)
-exports.onDeleteUser = functions.auth.user().onDelete(user.onDelete)
+exports.onCreateUser = func.auth.user().onCreate(user.onCreate)
+exports.onDeleteUser = func.auth.user().onDelete(user.onDelete)

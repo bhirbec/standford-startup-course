b2e0aa3cf803c38d5bd1ab8797ad64e485541a7f
Benoit Hirbec <benoit.hirbec@gmail.com>
2017-11-19 22:49:05 -0800

    Added favicon

--- app/templates/layout.jsx
+++ app/templates/layout.jsx
@@ -8,7 +8,7 @@
         function gtag(){dataLayer.push(arguments)};
     `}}></script>
 
-    <link rel="icon" href={asset("/public/img/favicon.jpg")} type="image/x-icon"/>
+    <link rel="icon" href={asset("/public/img/favicon.png")} type="image/x-icon"/>
     <meta charSet="utf-8" />
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />

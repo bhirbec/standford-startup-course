0c69952c1b49284af02fe7d4af49aeb1e9666d9e
Benoit Hirbec <benoit.hirbec@gmail.com>
2017-12-02 11:23:18 -0800

    update home page

--- app/client/home.js
+++ app/client/home.js
@@ -13,7 +13,7 @@ class Home extends React.Component {
     }
 
     render() {
-        return <div className="content-section-a">
+        return <div className="home content-section-a">
             <div className="intro-header">
                 <SocialButtons
                     shareUrl={`https://letsresume.com`}
@@ -21,7 +21,8 @@ class Home extends React.Component {
 
                 <div className="container intro-message">
                     <h1 className="home-h1">
-                        <span className="main-color">LetsResume</span><br />Résumés upvoted by the crowd.
+                        <span className="main-color">LetsResume</span><br />
+                        Make your résumé awesome.
                     </h1>
                     <Link to='/signup' onClick={this.onClickSignup.bind(this)}>
                         <button
@@ -40,7 +41,7 @@ class Home extends React.Component {
                         <div className="card-box">
                             <i className="material-icons">access_time</i>
                             <h4 className="card-title py-3 mbr-fonts-style display-5">
-                                <div className="main-color">Don't write your resume</div>
+                                <div className="main-color">Don't write your résumé</div>
                             </h4>
                             <p className="mbr-text mbr-fonts-style display-7">
                                 Just fill in the basics in under 3 minutes.<br />
@@ -80,40 +81,45 @@ class Home extends React.Component {
                 <h2>Watch the 1-minute how it works video</h2>
                 <iframe id="video"
                     src="https://www.youtube.com/embed/C5lhLSscV1A?rel=0&amp;controls=0&amp;showinfo=0"
-                    frameborder="0"
-                    allowfullscreen="true"></iframe>
+                    frameBorder="0"
+                    allowFullScreen="true"></iframe>
             </div>
 
-
-            {/*<div className="about-us">
+            <div className="about-us">
                 <div className="container">
-                    <div className="media-container-column mbr-white col-md-8">
-                        <h3 className="main-color">
-                            About Us
-                        </h3>
-                        <p className="mbr-text align-left pb-3 mbr-fonts-style display-5">
-                            Did you know that the <strong>average job seeker spends 1 to </strong>
-                            <strong>3 weeks writing their resume</strong>? While the average&nbsp;
-                            <strong>Recruiter spends just <u>6</u> seconds reviewing it</strong>.
-                        </p>
-                        <p>
-                            Imagine if you could radically transform that old, stale document [your resume],
-                            into a <strong>more powerful, authentic, and dynamic showcase</strong> that rapidly
-                            catapults your career to the next level.
-                        </p>
-                        <p>
-                            Using design thinking, we've re-imagined the entire experience from the bottom-up,
-                            to help millions of folks just like you around the world,&nbsp;
-                            <strong>use the power of their network (and the crowd)</strong>,&nbsp;
-                            <strong>to write your resume with you.</strong>
-                        </p>
-                        <p>
-                            <strong>LetsResume is <u>free</u> to join</strong>, and is currently
-                            being incubated at Stanford University.
-                        </p>
+                    <h2 className="main-color">Why LetsResume?</h2>
+                    <p>
+                        Did you know that <strong> job seekers spend </strong> on average&nbsp;
+                        <strong>3 weeks writing their résumé</strong>? While &nbsp;
+                        <strong>Recruiters spend just <u>6</u> seconds reviewing it</strong>.
+                        We asked ourselves why is everyone wasting their lives doing this exercise?
+                    </p>
+                    <div>
+                        <p>Recruiters care about 3 key elements with a potential hire:</p>
+                        <ul>
+                            <li>right skills</li>
+                            <li>cultural fit</li>
+                            <li>potential to grow</li>
+                        </ul>
                     </div>
+                    <p>
+                        The résumé is a biased version of yourself written by you. So
+                        how do Recruiters know you are who you say you are? We created
+                        a platform to help you change this. By asking your network
+                        to contribute to your profile you build a powerful, accurate,
+                        and crowd-supported identity that makes you look awesome. It's
+                        free to join!
+                    </p>
+                    <Link to='/signup' onClick={this.onClickSignup.bind(this)}>
+                        <button
+                            id="signup"
+                            type="button"
+                            className="btn btn-success">
+                            Sign up Free
+                        </button>
+                    </Link>
                 </div>
-            </div>*/}
+            </div>
 
             <div className="footer">
                 <div className="container">
